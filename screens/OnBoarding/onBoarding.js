7dd7663740134d95441882dd867ec4041aa319c1
Gurpreet <gs203181@gmail.com>
2022-03-11 22:55:15 +0100

    checking git

--- screens/OnBoarding/onBoarding.js
+++ screens/OnBoarding/onBoarding.js
@@ -1,82 +1,13 @@
 import React from  'react';
 import {t} from '../../constants/services/i18n/config';
-import { View,StyleSheet,Image  ,Button} from "react-native";
-import { COLORS, FONTS, SIZES ,OnBoardingData,icons, } from "../../constants";
+import { View,StyleSheet,Image,Animated,ImageBackground,Text  } from "react-native";
+import { COLORS, FONTS, SIZES ,OnBoardingData,icons, images,TextButton } from "../../constants";
+import { Button} from "../../components";
 
-import Onboarding from 'react-native-onboarding-swiper';
-
-const NextButton= ()=>{
-  return(
-    
-      <Button 
-      title='Sign in'
-      buttonStyle={{
-        backgroundColor: COLORS.black2,
-      }}
-      containerViewStyle={{
-        marginVertical: 10,
-        width: 70,
-        backgroundColor: COLORS.black2,
-      }}
-      textStyle={{ color: COLORS.white }}
-      {...props}/>
-  )      
-};
-const DotComponent = ({ isLight, selected }) =>{
- 
-  return (
-    <View
-      style={{
-        width: selected? 40 :5,
-        height: 5,
-        marginHorizontal: 4,
-        backgroundColor:selected ? COLORS.black2 : '#B5BFFF',
-        borderRadius:20,
-
-      }}
-    />
-  );
-};
 const OnBoarding = ({navigation}) =>{
-
-  
-    return(
-        <View style={{flex:1, backgroundColor:COLORS.white}}>
-        <Onboarding
-        onDone={()=>navigation.navigate('Welcome')}
-        showSkip={false}
-        bottomBarColor={COLORS.white}
-        NextButtonComponent={NextButton}
-        DotComponent={DotComponent}
-        
-        
-        
-
-
-        pages={[
-          {
-            backgroundColor: '#fff',
-            image: <Image source={icons.apple} />,
-            title: 'Onboarding',
-            subtitle: 'Done with React Native Onboarding Swiper',
-          },
-          {
-            backgroundColor: '#fff',
-            image: <Image source={icons.apple} />,
-            title: 'Onboarding',
-            subtitle: 'Done with React Native Onboarding Swiper',
-          },
-          {
-            backgroundColor: '#fff',
-            image: <Image source={icons.apple} />,
-            title: 'Onboarding',
-            subtitle: 'Done with React Native Onboarding Swiper',
-          },
-        ]}
-      />
-            
-                       
-        </View>
+ 
+  return(
+       <View></View>
     )
 }
 
